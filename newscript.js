b2b62aa11fc5ae52ec9681be9e839b643156b8e7
Susan Ashmore <scashmorecitcom@gmail.com>
2021-02-23 19:13:58 -0500

    functioning

--- newscript.js
+++ newscript.js
@@ -5,40 +5,31 @@ function headerTime() {
 }
 headerTime();
 
-//pull from storage 
-//Want to do a loop here? But this is okay?
-$("#entry1.description").val(localStorage.getItem("time"));
-$("#entry2.description").val(localStorage.getItem("time"));
-$("#entry3.description").val(localStorage.getItem("time"));
-$("#entry4.description").val(localStorage.getItem("time"));
-$("#entry5.description").val(localStorage.getItem("time"));
-$("#entry6.description").val(localStorage.getItem("time"));
-$("#entry7.description").val(localStorage.getItem("time"));
-$("#entry8.description").val(localStorage.getItem("time"));
-$("#entry9.description").val(localStorage.getItem("time"));
 
-
-$(".time-block").each(function () {
-    var rowTime = parseInt($(this).attr("id"));
+$(".description").each(function () {
+    var rowTime = parseInt($(this).parent().attr("id"));
 
     if (rowTime < moment().format("HH")) {
         $(this).addClass("past");
         $(this).removeClass("future");
         $(this).removeClass("present");
-    } else if (rowTime === moment().format("HH")) {
+    } else if (rowTime > moment().format("HH")) {
         $(this).removeClass("past");
-        $(this).addClass("present");
-        $(this).removeClass("future");
-    } else {
         $(this).removeClass("present");
-        $(this).removeClass("past");
         $(this).addClass("future");
+    } else {
+        $(this).addClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
     }
+    $(this).val(localStorage.getItem("time" + rowTime))
+    console.log(localStorage.getItem("time" + rowTime));
 })
 //Save button
 $(".saveBtn").on("click", function (event) {
     event.preventDefault();
     var entry = $(this).siblings(".description").val();
-    //var time = $(this).parent().attr("id");
-    localStorage.setItem("time", entry);
+    var time = $(this).parent().attr("id");
+    console.log(time);
+    localStorage.setItem("time" + time, entry);
 });
